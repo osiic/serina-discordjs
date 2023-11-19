91db60b82c9d601a73aa4ccc39d1c2bc8f744726
Osi ic <osiic.offcl@gmail.com>
2023-11-19 09:13:57 +0700

    fix(mongodb): fix setup mongodb

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 require("dotenv").config();
 const { Client, IntentsBitField } = require("discord.js");
 const { CommandHandler } = require("djs-commander");
-const { mongooseConnect } = require("./function/mongoose");
+const { mongoConnect } = require("./module/mongoConnect.js");
 const path = require("path");
 
 const client = new Client({
-  intents: [IntentsBitField.Flags.Guilds], // Your bot's intents
+  intents: [IntentsBitField.Flags.Guilds],
 });
-
-mongooseConnect();
 
 new CommandHandler({
-  client, // Discord.js client object | Required by default
-  commandsPath: path.join(__dirname, "commands"), // The commands directory
-  eventsPath: path.join(__dirname, "events"), // The events directory
-  validationsPath: path.join(__dirname, "validations"), // Only works if commandsPath is provided
-  testServer: process.env.SERVER_TEST, // To register guild-based commands (if it's not provided commands will be registered globally)
+  client,
+  commandsPath: path.join(__dirname, "commands"),
+  eventsPath: path.join(__dirname, "events"),
+  validationsPath: path.join(__dirname, "validations"),
+  testServer: process.env.SERVER_TEST,
 });
 
+mongoConnect();
 client.login(process.env.DISCORD_TOKEN);
--- src/schemas/stickSchema.js
+++ src/schemas/stickSchema.js
@@ -1,5 +1,14 @@
 const { model, Schema } = require("mongoose");
 
-let stickyScheme = new Schema({
-  Me,
+const stickyScheme = new Schema({
+  Message: { type: String },
+  ChannelID: { type: String },
+  LastMessage: { type: String },
+  LastMessageID: { type: String },
+  MaxCount: { type: Number, default: 6 },
+  CurrentCount: { type: Number, default: 0 },
 });
+
+module.exports = {
+  stickyScheme: model("stickyScheme", stickyScheme),
+};
